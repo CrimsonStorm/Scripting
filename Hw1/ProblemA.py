63ebe4d7181d3fd5ecc1008808eff7f5951363fd
CrimsonStorm <jlshaw@ucdavis.edu>
2018-01-17 22:10:02 -0800

    Adding hw1 files
    Hw1 files added to master branch

---
+++ Hw1/ProblemA.py
@@ -0,0 +1,168 @@
+#Polynomial class
+# args: List 
+class polynom(object):
+        poly = []
+        # initialize method
+        # sets instance variable poly to the object that was passed in.
+        def __init__(self,object):
+            self.poly = checkLeading(object)
+            
+        # + operator overload
+        # args: two polynomial lists, list1 and list2
+        # returns: new polynomial with the new polynomial instance variable inside.
+        def __add__(list1,list2):
+                copyList1 = checkBigger(list1.poly,list2.poly)
+                copyList2 = checkSmaller(list1.poly,list2.poly)
+                if(len(copyList1) == len(copyList2)):
+                        for i in range(0, len(copyList1)):
+                                copyList1[i] += copyList2[i]
+                else:
+                        for i in range(0, len(copyList2)):
+                                cl1StartPoint = len(copyList1) - len(copyList2)
+                                copyList1[cl1StartPoint+i] += copyList2[i]
+                newPoly = polynom(copyList1)
+                return newPoly
+        # - operator overload
+        # args: two polynomial lists, list1 and list2
+        # returns: new polynomial with the new polynomial instance variable inside. 
+        def __sub__(list1,list2):
+                copyList1 = list1.poly
+                copyList2 = list2.poly
+                if(len(copyList1) == len(copyList2)):
+                        for i in range(0, len(copyList1)):
+                                copyList1[i] -= copyList2[i]
+
+                        newPoly = polynom(copyList1)
+                else:
+                        if(len(copyList1) > len(copyList2)):
+                                for i in range(0, len(copyList2)):
+                                        cl1StartPoint = len(copyList1) - len(copyList2)
+                                        copyList1[cl1StartPoint+i] -= copyList2[i]
+                                newPoly = polynom(copyList1)
+                        elif(len(copyList1) < len(copyList2)):
+                                for i in range(0, len(copyList1)):
+                                        cl1StartPoint = len(copyList2) - len(copyList1)
+                                        copyList2[cl1StartPoint+i] -= copyList1[i]
+                                for i in range(0, len(copyList2)):
+                                        copyList2[i] = -copyList2[i]
+                                newPoly = polynom(copyList2)
+
+                return newPoly
+        
+        # * operator overload
+        # args: two polynomial lists, list1 and list2
+        # returns: new polynomial with the new polynomial instance variable inside.
+        def __mul__(list1,list2):
+                copyList1 = checkBigger(list1.poly,list2.poly)
+                copyList2 = checkSmaller(list1.poly,list2.poly)
+                if(len(copyList1) != 1) and (len(copyList2) != 1):
+                        newListLen = len(copyList1) + len(copyList2) - 1
+                        newList = [0] * newListLen
+                        for i in range(0, len(copyList1)):
+                                for j in range(0, len(copyList2)):
+                                        newList[i+j] = newList[i+j] + copyList1[i] * copyList2[j]
+
+                elif(len(copyList2) == 1):
+                        newList = [0] * len(copyList1)
+                        for i in range(0, len(copyList1)):
+                                newList[i] = copyList1[i] * copyList2[0]
+                                
+                else:
+                        copyList1[0] = copyList1[0] * copyList2[0]
+                        newList= copyList1       
+                newPoly = polynom(newList)
+                return newPoly
+                                        
+
+        # Integration method
+        # args: int a, int b
+        # integrates self.poly from a to b.
+        # returns: value of the integration
+        def intg(self,a,b):
+                totalInt = 0
+                for i in range(0,len(self.poly)):
+                        totalInt += 1.0/(len(self.poly)-i) * power(b,(len(self.poly)-i)) * self.poly[i] - 1.0/(len(self.poly)-i) * power(a,(len(self.poly)-i)) *  self.poly[i]
+
+                return totalInt
+                        
+        # Derivative method
+        # takes the current polynomial and does the derivative.
+        # returns: the list of the polynomial
+        def drv(self):
+                copyList = self.poly
+                for i in range(0, len(self.poly)):
+                        copyList[i] = copyList[i] * (len(self.poly) - i - 1)
+                newList = allButLast(copyList)
+                #newPoly = polynom(newList)
+                return newList                
+
+        # len operator overload
+        # returns: length of self.poly
+        def __len__(self):
+                return len(self.poly)
+
+# exponent method
+# returns: a to the power b
+def power(a,b):
+        newA = a
+        for i in range(1,b):
+                newA = newA*a
+
+        return newA
+
+# checkLeading method
+# checks the beginning of the list for leading 0s, doing so recursively until no 0s are in the front. calls a function to create the new list if needed.
+# args: a list
+# returns: the new list without leading zeroes.
+def checkLeading(list):
+        if(list[0] == 0):
+                newList = allButFirst(list)
+                list = checkLeading(newList)
+                return list
+        else:
+                return list
+# allButFirst method
+# Takes a list and returns the tail.
+# args: a list
+# returns: a new list without the head element.
+def allButFirst(list):
+        newList = []
+        for i in range(0, len(list)-1):
+                newList.append(list[i+1])
+                
+        return newList
+
+# allButLast method
+# Takes the given list and returns a list that has everything except the very last element.
+def allButLast(list):
+        newList = []
+        for i in range(0, len(list)-1):
+                newList.append(list[i])
+
+        return newList
+
+# checkBigger method
+# determines which list of the two has more elements.
+# args: list1, list2
+# returns: whichever list is bigger.
+def checkBigger(list1,list2):
+        if(len(list1)>len(list2)):
+                return list1
+        elif(len(list1) < len(list2)):
+                return list2
+        else:
+                return list1
+
+# checkSmaller method
+# determines which list of the two has less elements.
+# args: list1, list2
+# returns: whichever list is smaller.
+def checkSmaller(list1,list2):
+        if(len(list1)>len(list2)):
+                return list2
+        elif(len(list1) < len(list2)):
+                return list1
+        else:
+                return list2
+
+
---
+++ Hw1/ProblemB.py
@@ -0,0 +1,47 @@
+import os
+
+# filePairs method
+# returns a list of tuples that contain files that are the same within a certain number of bytes.
+# args: dtree, nBytes
+# returns: list
+def filePairs(dtree, nBytes):
+    totalTree = os.path.abspath(dtree)
+    allFiles = []
+    os.path.walk(totalTree,getFiles,allFiles)
+    myList = checkForDupes(allFiles,nBytes)
+    return myList
+
+# getFiles method
+# gets all of the files in a given file tree.
+# args: files list, directory root, list of paths
+def getFiles(files, rootDir, myList):
+    for aFile in myList:
+        pathName = rootDir + '\\' + aFile
+        if( os.path.isfile(pathName)):
+            files.append(pathName)
+
+# checkForDupes method
+# checks for any duplicate files in the directory tree and returns the pair as a tuple if there are any.
+# args: fileList, numBytes
+# returns: List of duplicate files
+def checkForDupes(fileList,numBytes):
+    returnList = []
+    for f in range(0,len(fileList)):
+        dupeFound = False
+        for i in range(f,len(fileList)):
+            if(f != i):
+                for x in range(1,numBytes):
+                    oFile = open(fileList[f])
+                    oFile2 = open(fileList[i])
+                    fd1 =  oFile.fileno()
+                    fd2 =  oFile2.fileno()
+                    check1 = os.read(fd1,x)
+                    check2 = os.read(fd2,x)
+                    if(check1 == check2):
+                        dupeFound = True
+                    else:
+                        dupeFound = False
+            if(dupeFound):
+                returnList.append((fileList[f],fileList[i]))
+
+    return returnList
