27f2a2f6e7bedaf06f428b9985fd87eb51b2c230
CrimsonStorm <jlshaw@ucdavis.edu>
2018-02-03 14:01:53 -0800

    added changes

--- Hw2/HwkII.py
+++ Hw2/HwkII.py
@@ -3,12 +3,12 @@ import socket
 
 
 def sysStart(hostList,portNum):
-    global sList = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)] * len(hostList)
-    for i in range(0,len(sList):
-        sList[i].bind(host[i],portNum)
+    sList = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)] * len(hostList)
+    for i in range(0,len(sList)):
+        sList[i].bind((hostList[i],portNum))
         sList[i].listen(1)
         connection,address = sList[i].accept()
-        
+        print 'made it here'
 
 def sysStop(hostList):
     for i in range(0,len(hostList)):
@@ -16,14 +16,18 @@ def sysStop(hostList):
 
 
 def dInit(hostList):
-    global allHosts = hostList[:]
+    global allHosts 
+    allHosts = hostList[:]
     
 
 def dread():
+	return 0
 
 def dwrite():
+	return 0
 
 def dopen():
+	return 0
 
 def dclose():
-    
+	return 0
--- Hw2/HwkIIServer.py
+++ Hw2/HwkIIServer.py
@@ -1,1 +1,6 @@
+from HwkII import sysStart
+
+myHosts = ['pc17.cs.ucdavis.edu', 'pc18.cs.ucdavis.edu']
+portNum = 1024
  
+sysStart(myHosts,portNum)
