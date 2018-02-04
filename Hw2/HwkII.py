5f87f6ae466c2f06ed47639104dc8f64914a89d9
CrimsonStorm <jlshaw@ucdavis.edu>
2018-02-03 18:20:48 -0800

    changed stuff

--- Hw2/HwkII.py
+++ Hw2/HwkII.py
@@ -3,31 +3,29 @@ import socket
 
 
 def sysStart(hostList,portNum):
-    sList = [socket.socket(socket.AF_INET, socket.SOCK_STREAM)] * len(hostList)
-    for i in range(0,len(sList)):
-        sList[i].bind((hostList[i],portNum))
-        sList[i].listen(1)
-        connection,address = sList[i].accept()
-        print 'made it here'
-
+        username = 'jlshaw'
+        password = ''
+        for i in hostList:
+                os.system('ssh %(username)s@%(i)s ' %{'username': username, 'i': i})
+                os.system('echo hello')
 def sysStop(hostList):
-    for i in range(0,len(hostList)):
-        sList[i].close()
+        for i in range(0,len(hostList)):
+                sList[i].close()
 
 
 def dInit(hostList):
-    global allHosts 
-    allHosts = hostList[:]
+        global allHosts 
+        allHosts = hostList[:]
     
 
 def dread():
-	return 0
+        return 0
 
 def dwrite():
-	return 0
+        return 0
 
 def dopen():
-	return 0
+        return 0
 
 def dclose():
-	return 0
+        return 0
