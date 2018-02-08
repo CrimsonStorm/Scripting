a042eeeb63fa2977ac06d682211f7a1449c1b0a9
CrimsonStorm <jlshaw@ucdavis.edu>
2018-02-07 20:55:50 -0800

    funcs work until 5th call?

--- Hw2/runClient.py
+++ Hw2/runClient.py
@@ -6,45 +6,77 @@ allHosts = []
 def dInit(hostList,portNum):
         #Tell the server(manager) to start sysStart with the hostList and portnum
         global allHosts
-	global s
+        global s
         map(lambda x: allHosts.append(x), hostList)
         #for each hostlist, connect to each port
-	s = [None]*len(hostList)
-	j = 0
-	for host in hostList:
-        	s[j] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        	s[j].connect((host,portNum))
-        	j = j + 1
+        s = [None]*len(hostList)
+        j = 0
+        for host in hostList:
+                s[j] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s[j].connect((host,portNum))
+                j = j + 1
 
-def dread(whichFile,byteNum = None):
+def dread(whichFile,byteNum = 0):
         #send request to server to read file
         #f = open(whichFile,r)
         assignedToPort = hash(whichFile) % len(allHosts)
-        s[assignedToPort].send('read ' + whichFile + ' ' + byteNum)
-
+        strByte = str(byteNum)
+        s[assignedToPort].send('read ' + whichFile + ' ' + strByte)
+        returnData = ''
+        while 1:
+                returnData = s[assignedToPort].recv(4096)
+                if(returnData!=None):
+                        break
+              
+        return returnData
 def dwrite(whichFile, whatToWrite):
         #send request to server to write file
         assignedToPort = hash(whichFile) % len(allHosts)
         s[assignedToPort].send('write ' + whichFile + ' ' + whatToWrite)
-
-def dopen(whichFile, permissions = 'r'):
+        while 1:
+                if(s[assignedToPort].recv(1024) == 'doneWriting'):
+                        break
+                
+def dopen(whichFile, permissions = 'a+'):
         # send request to server to open file
         assignedToPort = hash(whichFile) % len(allHosts)
         s[assignedToPort].send('open ' + whichFile + ' ' + permissions)
-
+        while 1:
+                if(s[assignedToPort].recv(1024) == 'doneOpening'):
+                        break
+                
 def dclose(whichFile):
         # send request to server to close file
         assignedToPort = hash(whichFile) % len(allHosts)
+	temp = 'close ' + whichFile
+	print temp
         s[assignedToPort].send('close ' + whichFile)
-
+        while 1:
+                if(s[assignedToPort].recv(1024) == 'doneClosing'):
+                        break
+                
 def main():
+<<<<<<< HEAD
+        dInit(['pc47.cs.ucdavis.edu','pc48.cs.ucdavis.edu'],2500)
+        dopen('testFile.txt')
+        #f = dread('testFile.txt')
+        #print f
+        #dwrite('testFile.txt','i am writing')
+        dclose('testFile.txt')
+        dopen('testFile.txt')
+        f = dread('testFile.txt')
+        print f
+        #dwrite('testFile2.txt','i am writing')
+        dclose('testFile.txt')
+=======
 	dInit(['pc47.cs.ucdavis.edu','pc48.cs.ucdavis.edu'],2500)
-	dopen('testFile.txt')
+	dopen('testFile.txt','w')
 	dwrite('testFile.txt','i am writing')
 	print 'closing'
 	dclose('testFile.txt')
 	print 'closed'
 
 
+>>>>>>> df4222c4839cde7b6f0e9f5c4c52034f746c649e
 if __name__ == '__main__':
-	main()
+        main()
--- Hw2/runServer.py
+++ Hw2/runServer.py
@@ -20,18 +20,57 @@ def main():
 
 	port = sys.argv[1]
 
-	lstn.bind(('',int(port)))
+	lstn.bind(('',int(port))) 
 	lstn.listen(1)
 
 	con, adr = lstn.accept()
-
+	f = None
+	
 	while(1):
-		data = con.recv(1024)
+<<<<<<< HEAD
+		data = con.recv(4096)
 		print 'data is ' + data
 		print 'here'
 		data = data.split()
 		print 'after'
-		print 
+		print data
+		sendData = ''
+		print f
+		if data != None:
+			if data[0] == 'read'  and f != None:
+				print 'reading'
+				if data[2] == '0':
+					sendData = f.read()
+					con.send(sendData)
+				else:
+					myInt = int(data[2])
+					sendData  = f.read(data[2])
+					con.send(sendData)
+			elif data[0] == 'write' and f != None:
+				strData = data[2:]
+				writeStr = ''
+				for i in strData:
+					writeStr += i
+				f.write(writeStr )
+				f.flush()
+				con.send('doneWriting')
+			elif data[0] == 'open'  and f == None:
+				f = open(data[1],data[2])
+				print 'opened'
+				con.send('doneOpening')
+			elif data[0] == 'close' and f != None:
+				f.close()
+				f = None
+				con.send('doneClosing')
+=======
+		data = con.recv(1024)
+		if data == '':
+			con.close()
+			break
+		print 'data is ' + data
+		print 'here'
+		data = data.split()
+		print 'after' 
 		print data
 		f = getFile(data[1])
 		if   data[0] == 'read'  and f != None:
@@ -42,12 +81,13 @@ def main():
 		elif data[0] == 'write' and f != None:
 			print 'writing'
 			data = [data[0], data[1], data[2:]]
-			f.write(data[2])
+			write(data[2])
 		elif data[0] == 'open'  and f == None:
 			f = open(data[1],data[2])
 		elif data[0] == 'close' and f != None:
 			fileList.remove(f)
 			f.close()
+>>>>>>> df4222c4839cde7b6f0e9f5c4c52034f746c649e
 
 if __name__ == '__main__':
     main()
