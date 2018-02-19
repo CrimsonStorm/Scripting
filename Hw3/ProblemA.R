599512ebd859142d5fa0190627fe8a18c2866042
CrimsonStorm <jlshaw@ucdavis.edu>
2018-02-19 01:59:30 -0800

    Current Hw3 done

---
+++ Hw3/ProblemA.R
@@ -0,0 +1,44 @@
+makeAscendNums <- function(vec){
+	returnClass = vec
+	if(!(all(diff(returnClass )>=0))){
+		stop("not nondecreasing")
+	}
+	if(all(diff(returnClass) >0)){
+		attr(returnClass , 'strictAscend') <- TRUE
+	}else{
+		attr(returnClass , 'strictAscend') <- FALSE
+	}
+	class(returnClass) <- "ascendNums"
+	returnClass 
+}
+
+"[<-.ascendNums" <- function(x,i,value){a = 
+	stop("read-only")
+}
+
+"+.ascendNums" <- function(x,x2){
+	xVec = c(x)
+	x2Vec = c(x2)
+	newVector = recursiveFunc(xVec ,x2Vec )
+	returnVec = makeAscendNums(newVector)
+	returnVec
+}
+
+recursiveFunc <- function(b,c){
+		totalVec = c()
+		if(length(b) > 0 && length(c) > 0){
+			minVal = min(b,c)
+			if(!is.na(match(minVal,b))){
+				b <- b[-1]
+			}else{
+				c <- c[-1]
+			}
+			totalVec <- c(totalVec,minVal)
+			totalVec = c(totalVec,recursiveFunc(b,c))
+		}else if(length(b) == 0){
+			totalVec = c(totalVec,c)
+		}else{
+			totalVec = c(totalVec,b)
+		}
+		totalVec
+}
