e6182934382d40c7b932e9cf29530eb1f6db6798
CrimsonStorm <jlshaw@ucdavis.edu>
2018-02-22 01:03:00 -0800

    fixed assignments to <-

--- Hw3/ProblemA.R
+++ Hw3/ProblemA.R
@@ -1,5 +1,5 @@
 makeAscendNums <- function(vec){
-	returnClass = vec
+	returnClass <- vec
 	if(!(all(diff(returnClass )>=0))){
 		stop("not nondecreasing")
 	}
@@ -12,33 +12,33 @@ makeAscendNums <- function(vec){
 	returnClass 
 }
 
-"[<-.ascendNums" <- function(x,i,value){a = 
+"[<-.ascendNums" <- function(x,i,value){
 	stop("read-only")
 }
 
 "+.ascendNums" <- function(x,x2){
-	xVec = c(x)
-	x2Vec = c(x2)
+	xVec <- c(x)
+	x2Vec <- c(x2)
 	newVector = recursiveFunc(xVec ,x2Vec )
 	returnVec = makeAscendNums(newVector)
 	returnVec
 }
 
 recursiveFunc <- function(b,c){
-		totalVec = c()
+		totalVec <- c()
 		if(length(b) > 0 && length(c) > 0){
-			minVal = min(b,c)
+			minVal <- min(b,c)
 			if(!is.na(match(minVal,b))){
 				b <- b[-1]
 			}else{
 				c <- c[-1]
 			}
 			totalVec <- c(totalVec,minVal)
-			totalVec = c(totalVec,recursiveFunc(b,c))
+			totalVec <- c(totalVec,recursiveFunc(b,c))
 		}else if(length(b) == 0){
-			totalVec = c(totalVec,c)
+			totalVec <- c(totalVec,c)
 		}else{
-			totalVec = c(totalVec,b)
+			totalVec <- c(totalVec,b)
 		}
 		totalVec
 }
