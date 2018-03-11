3bdec84808eb0316161f0b98ee5557785e961cc7
CrimsonStorm <jlshaw@ucdavis.edu>
2018-03-11 15:16:22 -0700

    Adding functions we need to make

--- Final Project/Rposim.R
+++ Final Project/Rposim.R
@@ -4,3 +4,58 @@
 # yield
 # hold
 #
+
+
+## Initialize global variables to be used.
+__init__ <- function(){
+
+
+
+
+
+
+}
+
+yield <- function(){
+
+
+
+
+}
+
+
+hold <- function(){
+
+
+
+
+
+
+
+}
+
+## Create a timer 
+now <- function(){
+
+
+
+}
+
+## Activate the function within the class that starts simulation.
+activate <- function(calssInst,classFunc){
+	doneSim = FALSE;
+	#Do code in new R initialiation
+	while(!doneSim){
+	}
+
+
+
+
+}
+
+simulate(untilTime){
+
+
+
+
+}
